31e6afb8f129f0a39c794ea1692953aa12ff3f32
ampey14 <ampeydanielOP@gmail.com>
2022-09-02 14:23:48 +0000

    5

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -20,12 +20,12 @@ const Activities = () => {
                 <tbody class="divide-y divide-gray-300">
                     <tr>
                     <td class="p-4 text-left">Arrival of Guests & Dignitaries</td>
-                    <td class="p-4">Musical Interlude</td>
+                    <td class="p-4 text-right">Musical Interlude</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Unveiling the New Phase of Credence</td>
-                    <td class="p-4">Credence Micro Credit HQ</td>
+                    <td class="p-4 text-right">Credence Micro Credit HQ</td>
                     </tr>
                 </tbody>
             </table>
@@ -39,37 +39,37 @@ const Activities = () => {
                 <tbody class="divide-y divide-gray-100">
                     <tr>
                     <td class="p-4 text-left">Musical Interlude</td>
-                    <td class="p-4">Musical Band</td>
+                    <td class="p-4 text-right">Musical Band</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Acknowledgement of Dignitaries</td>
-                    <td class="p-4">Ms. Jennifer J. Danquah</td>
+                    <td class="p-4 text-right">Ms. Jennifer J. Danquah</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Musical Interlude</td>
-                    <td class="p-4">Musical Band</td>
+                    <td class="p-4 text-right">Musical Band</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Introduction of Chairperson</td>
-                    <td class="p-4">Ms. Odelia Ntiamoah</td>
+                    <td class="p-4 text-right">Ms. Odelia Ntiamoah</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Chairperson's address</td>
-                    <td class="p-4">Chairperson</td>
+                    <td class="p-4 text-right">Chairperson</td>
                     </tr>
                     
                     <tr>
                     <td class="p-4 text-left">Credence Documentary</td>
-                    <td class="p-4">Media</td>
+                    <td class="p-4 text-right">Media</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Anniversary Speech</td>
-                    <td class="p-4">Mr. Ivan Avereyireh</td>
+                    <td class="p-4 text-right">Mr. Ivan Avereyireh</td>
                     </tr>
 
                 </tbody>
@@ -77,99 +77,99 @@ const Activities = () => {
             <br />
             <br />
             
-            <p className='text-3xl text-left'>
+            <p className='text-3xl text-left px-3'>
                 Testimonials
             </p>
             <br />
             <table class="min-w-full shadow-md rounded">
-                <tbody class="divide-y lg:divide-gray-100">
+                <tbody class="divide-y divide-gray-100">
+
                     <tr>
                     <td class="p-4 text-left">Friends of Credence</td>
-                    <td class="p-4">Mr. Richard Amuzu</td>
+                    <td class="p-4 text-right">Mr. Richard Amuzu</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Customer Reps</td>
-                    <td class="p-4">Mr. Frank Agyapong</td>
+                    <td class="p-4 text-right">Mr. Frank Agyapong</td>
                     </tr>
+                
                 </tbody>
-            </ table>
+            </table>
 
-
-            <p className='py-5'>
+            <p className='py-9'>
                 
             </p>
             <table class="min-w-full shadow-md rounded">
-                <tbody class="divide-y lg:divide-gray-100">
-
-                    <tr>
+                <tbody class="divide-y divide-gray-100">
+                <tr>
                     <td class="p-4 text-left">Unveiling of New Products/Service Lines</td>
-                    <td class="p-4">Team Credence</td>
+                    <td class="p-4 text-right">Team Credence</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Awards & Acknowledgements</td>
-                    <td class="p-4">Adeline Quarshie</td>
+                    <td class="p-4 text-right">Adeline Quarshie</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Musical Interlude</td>
-                    <td class="p-4">Music Band</td>
+                    <td class="p-4 text-right">Music Band</td>
                     </tr>
                     
                     <tr>
                     <td class="p-4 text-left">Profile Reading of Author</td>
-                    <td class="p-4">Ms. Adelina Qaurshie/ Ps. B. M. Nyarko</td>
+                    <td class="p-4 text-right">Ms. Adelina Qaurshie / Ps. B. M. Nyarko</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Testimonial of Author</td>
-                    <td class="p-4">Hon Albert Nakotey Tetteh</td>
+                    <td class="p-4 text-right">Hon Albert Nakotey Tetteh</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Purpose/Motivation/Synopsis of the Book</td>
-                    <td class="p-4">Author</td>
+                    <td class="p-4 text-right">Author</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Song Rendition</td>
-                    <td class="p-4">Minstrel Priscilla Gedjah</td>
+                    <td class="p-4 text-right">Minstrel Priscilla Gedjah</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Prayer for the Author</td>
-                    <td class="p-4">Apostle Dan Clad</td>
+                    <td class="p-4 text-right">Apostle Dan Clad</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Unveiling of the First Autographed Copies</td>
-                    <td class="p-4">Apostle Dan Clad</td>
+                    <td class="p-4 text-right">Apostle Dan Clad</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Musical Interlude</td>
-                    <td class="p-4">Music Band</td>
+                    <td class="p-4 text-right">Music Band</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Cutting of Anniversary Cake</td>
-                    <td class="p-4">Bishop Elizabeth Clad</td>
+                    <td class="p-4 text-right">Bishop Elizabeth Clad</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Closing Remarks</td>
-                    <td class="p-4">Chairperson</td>
+                    <td class="p-4 text-right">Chairperson</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Word of Appreciation</td>
-                    <td class="p-4">Ms. Gertrude Koomson</td>
+                    <td class="p-4 text-right">Ms. Gertrude Koomson</td>
                     </tr>
 
                     <tr>
                     <td class="p-4 text-left">Closing Prayer</td>
-                    <td class="p-4">Rev. Elton Dusi</td>
+                    <td class="p-4 text-right">Rev. Elton Dusi</td>
                     </tr>
 
                     <tr>
@@ -180,13 +180,15 @@ const Activities = () => {
                     <tr>
                     <td class="p-4 text-left">Departure</td>
                     </tr>
-
                 </tbody>
             </table>
+           
             </div>            
         </div>
     </div>
   )
 }
+
+
 
 export default Activities
